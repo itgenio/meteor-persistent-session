01324af9bf0682f31d65fadb201ae00291f246b3
afrokick <devbyru@gmail.com>
2022-06-25 14:48:31 +0300

    release

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "itgenio:persistent-session",
-  version: "0.4.6",
+  version: "0.4.7",
   summary: "Persistently store Session data on the client",
   git: "https://github.com/itgenio/meteor-persistent-session"
 });
